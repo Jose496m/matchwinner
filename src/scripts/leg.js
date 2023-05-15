4c8c68e9cd617fa124c05521cb9edcb3bfd51cf8
josephine olesen <jose496m@stud.kea.dk>
2023-05-15 13:19:18 +0200

    leg mobil version næsten done

--- src/scripts/leg.js
+++ src/scripts/leg.js
@@ -1,27 +1,7 @@
-var menuItems = document.querySelectorAll(".menu li");
+const parents = document.querySelectorAll(".parent");
 
-menuItems.forEach(function (menuItem) {
-  var submenu = menuItem.querySelector(".submenu");
-
-  menuItem.addEventListener("click", function (event) {
-    event.stopPropagation();
-
-    if (submenu.style.display === "block") {
-      submenu.style.display = "none";
-    } else {
-      submenu.style.display = "block";
-    }
-  });
-
-  submenu.addEventListener("click", function (event) {
-    event.stopPropagation();
-  });
-});
-
-document.addEventListener("click", function () {
-  var dropdownMenus = document.querySelectorAll(".submenu");
-
-  dropdownMenus.forEach(function (submenu) {
-    submenu.style.display = "none";
+parents.forEach((parent) => {
+  parent.addEventListener("click", function () {
+    this.classList.toggle("active");
   });
 });
