02dc7b8000d94ecf8b826245edc9f97ed1f6ef56
josephine olesen <jose496m@stud.kea.dk>
2023-05-15 11:27:44 +0200

    leg js

--- src/scripts/leg.js
+++ src/scripts/leg.js
@@ -1,37 +1,27 @@
-document.addEventListener("click", function (event) {
-  var clickedElement = event.target;
+var menuItems = document.querySelectorAll(".menu li");
+
+menuItems.forEach(function (menuItem) {
+  var submenu = menuItem.querySelector(".submenu");
+
+  menuItem.addEventListener("click", function (event) {
+    event.stopPropagation();
+
+    if (submenu.style.display === "block") {
+      submenu.style.display = "none";
+    } else {
+      submenu.style.display = "block";
+    }
+  });
+
+  submenu.addEventListener("click", function (event) {
+    event.stopPropagation();
+  });
+});
+
+document.addEventListener("click", function () {
   var dropdownMenus = document.querySelectorAll(".submenu");
 
   dropdownMenus.forEach(function (submenu) {
-    var menuItem = submenu.parentNode;
-    if (menuItem.contains(clickedElement)) {
-      // Klikket inden for dropdown-menuen eller dens forældrelementer, intet skal gøres
-    } else {
-      // Klikket uden for dropdown-menuen, skjul den
-      submenu.style.display = "none";
-    }
-  });
-});
-
-document.querySelectorAll(".menu li").forEach(function (menuItem) {
-  menuItem.addEventListener("click", function (event) {
-    event.stopPropagation();
-    var submenu = this.querySelector(".submenu");
-    submenu.style.display = "block";
-  });
-});
-
-document.querySelectorAll(".menu li").forEach(function (menuItem) {
-  menuItem.addEventListener("click", function (event) {
-    event.stopPropagation();
-    var submenu = this.querySelector(".submenu");
-    submenu.style.display = "block";
-
-    // Rul til menuen
-    var menuOffsetTop = menuItem.offsetTop;
-    window.scrollTo({
-      top: menuOffsetTop,
-      behavior: "smooth",
-    });
+    submenu.style.display = "none";
   });
 });
