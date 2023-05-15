e433f8e542687e5465419898382e1820976a7299
josephine olesen <jose496m@stud.kea.dk>
2023-05-15 11:14:36 +0200

    leg

--- src/scripts/leg.js
+++ src/scripts/leg.js
@@ -1,3 +1,26 @@
+document.addEventListener("click", function (event) {
+  var clickedElement = event.target;
+  var dropdownMenus = document.querySelectorAll(".submenu");
+
+  dropdownMenus.forEach(function (submenu) {
+    var menuItem = submenu.parentNode;
+    if (menuItem.contains(clickedElement)) {
+      // Klikket inden for dropdown-menuen eller dens forældrelementer, intet skal gøres
+    } else {
+      // Klikket uden for dropdown-menuen, skjul den
+      submenu.style.display = "none";
+    }
+  });
+});
+
+document.querySelectorAll(".menu li").forEach(function (menuItem) {
+  menuItem.addEventListener("click", function (event) {
+    event.stopPropagation();
+    var submenu = this.querySelector(".submenu");
+    submenu.style.display = "block";
+  });
+});
+
 document.querySelectorAll(".menu li").forEach(function (menuItem) {
   menuItem.addEventListener("click", function (event) {
     event.stopPropagation();
