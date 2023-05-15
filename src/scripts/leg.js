3f9b538ae62200b9ca65c190a9f1270591f11947
josephine olesen <jose496m@stud.kea.dk>
2023-05-15 11:24:29 +0200

    leg

--- src/scripts/leg.js
+++ src/scripts/leg.js
@@ -1,26 +1,23 @@
-document.querySelectorAll(".menu li").forEach(function (menuItem) {
-  var submenu = menuItem.querySelector(".submenu");
-
-  menuItem.addEventListener("click", function (event) {
-    event.stopPropagation();
-
-    if (submenu.style.display === "block") {
-      submenu.style.display = "none";
-    } else {
-      submenu.style.display = "block";
-    }
-  });
-
-  submenu.addEventListener("click", function (event) {
-    event.stopPropagation();
-  });
-});
-
-document.addEventListener("click", function () {
+document.addEventListener("click", function (event) {
+  var clickedElement = event.target;
   var dropdownMenus = document.querySelectorAll(".submenu");
 
   dropdownMenus.forEach(function (submenu) {
-    submenu.style.display = "none";
+    var menuItem = submenu.parentNode;
+    if (menuItem.contains(clickedElement)) {
+      // Klikket inden for dropdown-menuen eller dens forældrelementer, intet skal gøres
+    } else {
+      // Klikket uden for dropdown-menuen, skjul den
+      submenu.style.display = "none";
+    }
+  });
+});
+
+document.querySelectorAll(".menu li").forEach(function (menuItem) {
+  menuItem.addEventListener("click", function (event) {
+    event.stopPropagation();
+    var submenu = this.querySelector(".submenu");
+    submenu.style.display = "block";
   });
 });
 
