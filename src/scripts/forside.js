768a58a093778c4a61e5138826a33d99492383fe
Nicoline Meulengracht <nico795z@stud.kea.dk>
2023-05-23 10:55:17 +0200

    database der virker

--- src/scripts/forside.js
+++ src/scripts/forside.js
@@ -1,13 +1,13 @@
 //https://lhlqzgpvydlodjfvvbru.supabase.co
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxobHF6Z3B2eWRsb2RqZnZ2YnJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ0Nzk0MDMsImV4cCI6MjAwMDA1NTQwM30.M5McXqusKfxEHBGbSXw15LL7maXShpRl7-Q-7oMgWeI
 
-fetch("https://lhlqzgpvydlodjfvvbru.supabase.co/rest/v1/matchwinner", {
+fetch("https://lhlqzgpvydlodjfvvbru.supabase.co/rest/v1/Matchwinner", {
   method: "GET",
   headers: { apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxobHF6Z3B2eWRsb2RqZnZ2YnJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ0Nzk0MDMsImV4cCI6MjAwMDA1NTQwM30.M5McXqusKfxEHBGbSXw15LL7maXShpRl7-Q-7oMgWeI" },
 })
   .then((res) => res.json())
   .then(showData);
 
-function showData(matchwinner) {
-  console.log(matchwinner);
+function showData(Matchwinner) {
+  console.log(Matchwinner);
 }
